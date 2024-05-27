e821a921fd214d6d612ba83acd80ee4eca747516
Aaditya <aadityajujagar@gmail.com>
2024-05-27 14:15:48 +0530

    map reload problem resolved

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,4 +1,5 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useEffect } from "react";
 
 const center = {
   lat: 17.6679871,
@@ -6,8 +7,13 @@ const center = {
 };
 
 const Map = () => {
+  useEffect(() => {
+    console.log("API Key:", import.meta.env.VITE_API_KEY);
+  }, []);
+
   const api_key = import.meta.env.REACT_APP_API_KEY;
   console.log(api_key);
+
   return (
     <LoadScript googleMapsApiKey={api_key}>
       <GoogleMap
